2c540ecf6265f0a0453e4b623d2374dff0311b01
igal abachi <igalk474@gmail.com>
2021-10-19 02:33:23 +0300

    axios

--- services/api.service.tsx
+++ services/api.service.tsx
@@ -28,13 +28,63 @@ export const widget = (res :SWRResponse<any, any>, renderer:any) => {
     return renderer(res.data);
 }
 
+//
+// const axiosClient = axios.create();
+//
+// axiosClient.defaults.baseURL = 'https://example.com/api/v1';
+//
+//
+//axiosClient.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+//
+// axiosClient.defaults.headers = {
+//     'Content-Type': 'application/json',
+//     Accept: 'application/json' // 'Accept': 'application/json, text/plain, */*'
+// };
+//"Accept": 'application/json, text/plain, */*',
+//    "Content-Type": 'application/json;charset=UTF-8' //
+//'Content-Type': 'application/x-www-form-urlencoded'
+//
+// //All request will wait 2 seconds before timeout
+// axiosClient.defaults.timeout = 2000;//1000-2500 , default=0(no timeout)
+//
+// axiosClient.defaults.withCredentials = true;
+//
+//make hooks:
+// export function getRequest(URL) {
+//     return axiosClient.get(`/${URL}`).then(response => response);
+// }
+//
+// export function postRequest(URL, payload) {
+//     return axiosClient.post(`/${URL}`, payload).then(response => response);
+// }
+//
+// export function patchRequest(URL, payload) {
+//     return axiosClient.patch(`/${URL}`, payload).then(response => response);
+// }
+//
+// export function deleteRequest(URL) {
+//     return axiosClient.delete(`/${URL}`).then(response => response);
+// }
+// axios.interceptors.response.use(function (response) {
+//     //Dispatch any action on success
+//     return response;
+// }, function (error) {
+//     if(error.response.status === 401) {
+//         //Add Logic to
+//         //1. Redirect to login page or
+//         //2. Request refresh token
+//     }
+//     return Promise.reject(error);
+// });
 
-//Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
-//Axios.defaults.withCredentials = true;
+
+
+
+
 
 //https://dev.to/harshmangalam/complete-setup-of-nextjs-swr-axios-and-material-ui-with-ssr-for-your-upcomming-projects-25b0
 
-
+//https://dev.to/nilanth/how-to-use-axios-in-an-optimized-and-scalable-way-with-react-518n
 
 export const getAllBlogContent = () => {
 
@@ -44,3 +94,5 @@ export const getAllBlogContent = () => {
 export const getPostById = (id:number) => {
 
 };
+
+
